66f0a2763c7ea26c553d7fb96474715c6942a7ef
Arnav Verma <arnav809verma@gmail.com>
2025-10-28 23:54:40 +0530

    trying stuff

--- dataset_code.py
+++ dataset_code.py
@@ -2,9 +2,26 @@ from datasets import load_dataset
 
 dataset = load_dataset("dair-ai/emotion")
 
-# Split
+#split into train test
 train_data = dataset["train"]
 test_data = dataset["test"]
 
-# Example sample
+#tried printing stuff
 print(train_data[0])
+
+#tain test ts
+train_texts = [x['text'] for x in train_data]
+train_labels = [x['label'] for x in train_data]
+
+test_texts = [x['text'] for x in test_data]
+test_labels = [x['label'] for x in test_data]
+
+#try printing stuff to see
+print(train_texts[3])
+print(train_labels[3])
+
+#emotions
+emotion_names = dataset["train"].features["label"].names
+print(emotion_names)
+# ['sadness', 'joy', 'love', 'anger', 'fear', 'surprise']
+
